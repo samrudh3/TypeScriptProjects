dc7943df66d2e3d1466dd05e092efc0f9c6f799d
samrudh <samrudhgr3@gmail.com>
2024-10-16 23:34:28 +0530

    TypeScript with Build Process and Type of Variables

---
+++ typescript/src/Challenges.ts
@@ -0,0 +1,13 @@
+let cityName : string = "Bengaluru"
+console.log(cityName.toUpperCase())
+
+let age : number = 15
+console.log(age + 3)
+
+let isAdult : boolean = age >=18
+console.log(isAdult)
+
+// cityName = 20 // Locally TypeScript will work But in Build it Won't
+// age = "Twenty"
+
+console.log(cityName, age)
---
+++ typescript/src/Test.ts
@@ -0,0 +1,16 @@
+console.log("First TypeScript")
+
+let fullName : string = "John wick"
+fullName = "hello John"
+// fullName = 30 Not Possible
+console.log(fullName.toUpperCase())
+
+let value : number = 30
+console.log(value)
+// value = 'jwgdw' Not Possible
+
+value = value - 3;
+console.log("New Value = "+ value)
+
+let isTrue : boolean = true
+console.log(isTrue)
